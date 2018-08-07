06153861be654f684f27040e32b56484fea59e93
Nathan Tiedt <decafchild@gmail.com>
2018-08-07 15:05:58 -0600

    this point

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,8 +5,9 @@
 # TODO: also install aws cli tools
 ##
 
-ln -sf $(pwd)/scripts/ipseed /usr/local/bin/ipseed
-ln -sf $(pwd)/scripts/aws_session /usr/local/bin/aws_session
-ln -sf $(pwd)/scripts/ip /usr/local/bin/ip
+mkdir ~/.node_modules  # needed for user object for bitbucket
+#ln -sf $(pwd)/scripts/ipseed /usr/local/bin/ipseed
+#ln -sf $(pwd)/scripts/aws_session /usr/local/bin/aws_session
+#ln -sf $(pwd)/scripts/ip /usr/local/bin/ip
 
 
--- src/bit/js/git.js
+++ src/bit/js/git.js
@@ -7,14 +7,14 @@ module.exports = {
   publish: publish,
 };
 
-function pull () {
-  shell.exec('git pull');
-}
-
 function checkout ( branch, options='' ) {
   shell.exec(`git checkout ${options} ${branch}`);
 }
 
 function publish ( branch ) {
   shell.exec(`git push --set-upstream origin ${branch}`);
+}
+
+function pull () {
+  shell.exec('git pull');
 }
