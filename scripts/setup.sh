aae25771c7c994432d793e412c75a44d2af27fb2
Nathan Tiedt <decafchild@gmail.com>
2018-09-01 09:15:56 -0600

    psql functions in place

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
+DIRECTORY=~/.node_modules
+
 ##
 # setup script for rz_cli tools!
 # TODO: also install aws cli tools
 ##
-
-mkdir ~/.node_modules  # needed for user object for bitbucket
-#ln -sf $(pwd)/scripts/ipseed /usr/local/bin/ipseed
-#ln -sf $(pwd)/scripts/aws_session /usr/local/bin/aws_session
-#ln -sf $(pwd)/scripts/ip /usr/local/bin/ip
+if [ ! -d "$DIRECTORY" ]; then
+  mkdir ~/.node_modules  # needed for user object for bitbucket
+fi
 
 
