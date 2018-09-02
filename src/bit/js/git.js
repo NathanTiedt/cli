e48437de84e3315273ee34acfd97f66448f3e09e
Nathan Tiedt <decafchild@gmail.com>
2018-09-01 18:00:29 -0600

    feature now opens a pull-request page in browser

--- src/bit/js/git.js
+++ src/bit/js/git.js
@@ -1,14 +1,26 @@
 
 const shell = require('shelljs');
 
+let url = 'https://bitbucket.org/redzoneco/${repo}/pull-requests/new?source=${source}&dest=${destination}';
+const BASE_URL = 'https://bitbucket.org/redzoneco/';
+
 module.exports = {
   checkout: checkout,
-  pull: pull,
+  createPullRequest: createPullRequest,
   publish: publish,
+  pull: pull,
+  push: push,
 };
 
 function checkout ( branch, options='' ) {
   shell.exec(`git checkout ${options} ${branch}`);
+}
+
+function createPullRequest (source, destination) {
+  let repo = shell.exec('basename `git rev-parse --show-toplevel`').replace('\n', '');
+  let url = `${BASE_URL}${repo}/pull-requests/new?source=${source}&dest=${destination}`;
+  console.log(`url ${url}`);
+  shell.exec(`open "${url}"`);
 }
 
 function publish ( branch ) {
@@ -17,4 +29,8 @@ function publish ( branch ) {
 
 function pull () {
   shell.exec('git pull');
+}
+
+function push () {
+  shell.exec('git push');
 }
