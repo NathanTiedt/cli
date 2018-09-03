df12e4cac07dcf81d733e89f8a83273c11bd050b
Nathan Tiedt <decafchild@gmail.com>
2018-09-03 09:16:17 -0600

    setup_aws is complete

--- scripts/setup_aws.sh
+++ scripts/setup_aws.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+configInfo=(""
+"We need info from AWS for this step."
+"Secret Key and Access Key can be found in AWS Console"
+"Go to AWS Console -> IAM -> Users -> <YourUser> -> Security Credentials Tab -> Access Keys Section -> Create New Access Keys"
+"Default Region should be 'us-east-1'"
+"Default Output should be 'json'"
+"")
+
 function checkConfigs () {
   if [[ $(grep -R '[default]' ~/.aws/) ]]; then
     echo "AWS Installed and Configured"
@@ -17,8 +25,16 @@ function installAWS () {
   pip install awscli --upgrade --user
 }
 
+function runAwsConfigure () {
+  printf "%s\n" "${configInfo[@]}"
+  read -n 1 -s -r -p "Get Access Keys and Press Any Key to Continue"
+  aws configure
+}
+
 if which aws; then
   checkConfigs
+  runAwsConfigure
 else
   installAWS
+  runAwsConfigure
 fi
