fe5153fb9155c8e394805731fa29f8f33b883746
Nathan Tiedt <decafchild@gmail.com>
2018-09-03 08:21:13 -0600

    check configs done setup_aws.sh

--- scripts/setup_aws.sh
+++ scripts/setup_aws.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
-if ! which pip; then
-  echo "No Pip!"
+function checkConfigs () {
+  if [[ $(grep -R '[default]' ~/.aws/) ]]; then
+    echo "AWS Installed and Configured"
+    exit 0
+  fi
+}
+
+function installAWS () {
+  PIP=$(which pip) || $(which pip3)
+  if [[ -z "$PIP" ]]; then
+    echo "pip isn't installed"
+    echo "installing python to get pip"
+    brew install python
+  fi
+  pip install awscli --upgrade --user
+}
+
+if which aws; then
+  checkConfigs
+else
+  installAWS
 fi
